2340135fcecf9f08034b338b7bb37a797918219f
narinn-star <skfls2618@naver.com>
2020-04-29 03:17:03 +0900

    2주차 예제

---
+++ 2 Week/20203179 이나린_예제1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20203179_이나린_예제1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iData01 = 3;
+            label1.Text = "결과는 " + iData01 + "입니다";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double dData01 = -27.51;
+            dData01 = 3.5;
+            label1.Text = "결과는 " + dData01 + "입니다";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sData01 = "역삼동 할머니";
+            sData01 = "홍길동";
+            label1.Text = "결과는 " + sData01 + "입니다";
+        }
+    }
+}
---
+++ 2 Week/20203179 이나린_예제2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20203179_이나린_예제2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int iData01 = int.Parse(textBox1.Text);
+                label1.Text = "결과는 " + iData01 + "입니다";
+            }
+            catch
+            {
+                label1.Text = "잘못된 실행결과입니다";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double dData01 = double.Parse(textBox1.Text);
+                label1.Text = "결과는 " + dData01 + "입니다";
+            }
+            catch
+            {
+                label1.Text = "잘못된 실행결과입니다";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sData01 = textBox1.Text;
+                label1.Text = "결과는 " + sData01 + "입니다";
+            }
+            catch
+            {
+                label1.Text = "잘못된 실행결과입니다";
+            }
+        }
+    }
+}

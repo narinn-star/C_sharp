aac3a019e8ed73566e0bd4954ca664411cbbf69a
narinn-star <skfls2618@naver.com>
2020-06-11 22:55:25 +0900

    11주차 예제

--- 11 Week/20203179 이나린_예제3/Form1.cs
+++ 11 Week/20203179 이나린_예제3/Form1.cs
@@ -25,7 +25,20 @@ namespace _20203179_이나린_예제3
 
         private void button1_Click(object sender, EventArgs e)
         {
+            bool Valid = true;
+            string contents = textBox1.Text;
 
+            foreach (string word in Xwords)
+            {
+                if (contents.IndexOf(word) >= 0)
+                {
+                    Valid = false;
+                    MessageBox.Show(word + "은(는) 금지어입니다", "결과");
+                    break;
+                }
+                
+            }
+            if (Valid == true) MessageBox.Show("정상적인 게시물입니다", "결과");
         }
     }
 }
